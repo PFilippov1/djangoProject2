afe9ebf54362796022b8cb28069a056085ddd755
Pavel Filippov <p.filippov2.20@gmail.com>
2022-03-28 17:09:41 +0300

    Schedule + login

--- schedule/urls.py
+++ schedule/urls.py
@@ -7,5 +7,9 @@ app_name = 'schedule'
 urlpatterns = [
     path(r'', views.index, name='index'),
     re_path(r'^([0-9]+)/$', views.detail, name='detail'),
-    re_path(r'^([0-9]+)/answer/$', views.answer, name='answer')
+    re_path(r'^([0-9]+)/answer/$', views.answer, name='answer'),
+    re_path(r'^register/$', views.RegisterFormView.as_view()),
+    re_path(r'^login/$', views.LoginFormView.as_view()),
+    re_path(r'^logout/$', views.LogoutView.as_view()),
+    re_path(r'^password-change/', views.PasswordChangeView.as_view()),
 ]
